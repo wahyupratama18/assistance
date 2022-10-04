b82044d0b888e2bee9b08e1d9436f7cf687662d6
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2022-10-05 06:42:23 +0700

    Initial commit

---
+++ database/migrations/2022_09_23_031224_create_course_student_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Course;
+use App\Models\Student;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_student', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Student::class)->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignIdFor(Course::class)->nullable()->constrained()->cascadeOnUpdate()->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_student');
+    }
+};
